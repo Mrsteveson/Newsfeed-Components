d060266002fb7257762685026f4510803e7a1c38
mrsteveson <patrick.steveson727@gmail.com>
2019-03-06 15:25:04 -0600

    created the reference and click event listener in menu.js and updated a few articles about batman into the html

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelectorAll('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.forEach(function(button){
+      console.log(button);
+    });
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    console.log('expand');
   }
 }
 
@@ -26,4 +31,48 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+
+
+
+
+
+
+
+
+
+
+// Guided Lecture Code for reference
+
+// // Step 3: Build out the Panel base class (clases are hoisted)
+
+// class Panel {
+//   constructor(panel) {
+//     this.panel = panel;
+//     this.panelButtons = this.panel.querySelector('.panel-buttons');
+//     this.panelBtnOpen = this.panel.querySelector('.panel-btn-open');
+//     this.panelBtnClose = this.panel.querySelector('.panel-btn-close');
+//     this.panelContent = this.panel.querySelector('.panel-content');
+    
+//     // Explicit binding of this
+//     //this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
+    
+//     // this is NOT bound to the arrow and therefore looks up and finds the this keyword in the constructor
+//     this.panelButtons.addEventListener('click', () => this.togglePanel());
+//   }
+//   // methods
+//   togglePanel() {
+//     this.panelBtnOpen.classList.toggle('hide-btn');
+//     this.panelBtnClose.classList.toggle('hide-btn');
+//     this.panelContent.classList.toggle('toggle-on');
+//   }
+// }
+
+// // Step 1: Get the original DOM elements
+// const panels = document.querySelectorAll('.panel');
+
+// // Step 2: Return a newly constructed DOM element 
+// panels.forEach(function(panel){
+//   return new Panel(panel);
+// });
